d3e246ace4a83145f44634abf81ec6a8262b841e
GurunathanNataraj <gurunathannataraj@gmail.com>
2019-07-09 22:13:20 +0530

    first commit

---
+++ src/test/java/SeleniumProject/Class1Test.java
@@ -0,0 +1,16 @@
+package SeleniumProject;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	@Test
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	@Test	
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+}
---
+++ src/test/java/SeleniumProject/Class2Test.java
@@ -0,0 +1,16 @@
+package SeleniumProject;
+
+import org.testng.annotations.Test;
+
+public class Class2Test {
+	@Test
+	public void method3()
+	{
+		System.out.println("Method3");
+	}
+	@Test
+	public void method4()
+	{
+		System.out.println("Method4");
+	}
+}
